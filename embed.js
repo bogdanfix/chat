5acd130e2605e8dda33819c94e11abfa35831e8d
BogdanFix <stalker1894@gmail.com>
2023-06-09 17:49:37 +0600

    added a full-page embed chat window;

---
+++ embed.js
@@ -0,0 +1,22 @@
+const embedParent = document.getElementById('impact-embed-parent')
+const scriptTag = document.currentScript
+
+const chat = document.createElement('div')
+chat.setAttribute('id', 'impact-embed-child')
+
+chat.style.display = 'block'
+chat.style.position = 'relative'
+chat.style.width = '100%'
+chat.style.height = '80vh'
+chat.style.borderWidth = '1px'
+chat.style.borderStyle = 'solid'
+chat.style.borderColor = '#DDDDDD'
+
+embedParent.appendChild(chat)
+
+chat.innerHTML = `<iframe
+src="https://impact.bfix.dev/chatbot/${scriptTag.id}/embed"
+width="100%"
+height="100%"
+frameborder="0"
+></iframe>`
