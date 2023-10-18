a0064e18446dbff30c67b1aa0669b1dc8fb6287a
BogdanFix <stalker1894@gmail.com>
2023-10-18 18:27:15 +0600

    wrapped the code into async func; trigger the func only after page has loaded;

--- embed.js
+++ embed.js
@@ -1,22 +1,43 @@
-const embedParent = document.getElementById('impact-embed-parent')
-const scriptTag = document.currentScript
+async function embedImpactOnpage() {
 
-const chat = document.createElement('div')
-chat.setAttribute('id', 'impact-embed-child')
+	const embedParent = document.getElementById('impact-embed-parent')
 
-chat.style.display = 'block'
-chat.style.position = 'relative'
-chat.style.width = '100%'
-chat.style.height = '80vh'
-chat.style.borderWidth = '1px'
-chat.style.borderStyle = 'solid'
-chat.style.borderColor = '#DDDDDD'
+	if (!embedParent) {
 
-embedParent.appendChild(chat)
+		console.log( 'impactai: embed parent not found' )
 
-chat.innerHTML = `<iframe
-src="https://getimpact.ai/chatbot/${scriptTag.id}/embed"
-width="100%"
-height="100%"
-frameborder="0"
-></iframe>`
+		return;
+	}
+
+	const scriptTag = document.currentScript
+
+	const chat = document.createElement('div')
+	chat.setAttribute('id', 'impact-embed-child')
+
+	chat.style.display = 'block'
+	chat.style.position = 'relative'
+	chat.style.width = '100%'
+	chat.style.height = '80vh'
+	chat.style.borderWidth = '1px'
+	chat.style.borderStyle = 'solid'
+	chat.style.borderColor = '#DDDDDD'
+
+	embedParent.appendChild(chat)
+
+	chat.innerHTML = `<iframe
+	src="https://getimpact.ai/chatbot/${scriptTag.id}/embed"
+	width="100%"
+	height="100%"
+	frameborder="0"
+	></iframe>`
+}
+
+if (document.readyState === "complete") {
+
+  embedImpactOnpage();
+
+} else {
+
+  window.addEventListener('load', embedImpactOnpage());
+
+}
