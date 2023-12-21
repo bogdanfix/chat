f3a848af4d0436e10ec32a297a50f32e1845199e
BogdanFix <stalker1894@gmail.com>
2023-12-21 20:20:50 +0600

    added ru version of embed scripts;

---
+++ ru/embed.js
@@ -0,0 +1,43 @@
+async function embedImpactOnpage() {
+
+	const embedParent = document.getElementById('impact-embed-parent')
+
+	if (!embedParent) {
+
+		console.log( 'impactai: embed parent not found' )
+
+		return;
+	}
+
+	const scriptTag = document.currentScript
+
+	const chat = document.createElement('div')
+	chat.setAttribute('id', 'impact-embed-child')
+
+	chat.style.display = 'block'
+	chat.style.position = 'relative'
+	chat.style.width = '100%'
+	chat.style.height = '80vh'
+	chat.style.borderWidth = '1px'
+	chat.style.borderStyle = 'solid'
+	chat.style.borderColor = '#DDDDDD'
+
+	embedParent.appendChild(chat)
+
+	chat.innerHTML = `<iframe
+	src="https://app.wisechat.ru/chatbot/${scriptTag.id}/embed"
+	width="100%"
+	height="100%"
+	frameborder="0"
+	></iframe>`
+}
+
+if (document.readyState === "complete") {
+
+  embedImpactOnpage();
+
+} else {
+
+  window.addEventListener('load', embedImpactOnpage());
+
+}
